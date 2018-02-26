1d8032d4d07d210f050e1926d97055a46b3900e4
svrzii <msvrznjak@outlook.com>
2018-02-26 11:09:47 +0100

    Update project for opencv

--- app/src/main/java/com/example/matejsvrznjak/ms_scanner/MainActivity.java
+++ app/src/main/java/com/example/matejsvrznjak/ms_scanner/MainActivity.java
@@ -2,8 +2,20 @@ package com.example.matejsvrznjak.ms_scanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.i("opencv", "opencv initialization failed");
+
+        } else {
+            Log.i("opencv", "opencv initialization successfull");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
